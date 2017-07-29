353c2010203551718d58162166667f43447197c3
zero <zerolee18@gmail.com>
2017-07-29 21:14:25 +0800

    GHand documentation

--- Devices/GHand/sfingerThumb.m
+++ Devices/GHand/sfingerThumb.m
@@ -1,11 +1,9 @@
 function s = sfingerThumb(n, p)
 % Description: move two joints of Thumb with specific position and time;
-% Input:  	[LINK], handle of serial device
 % 			[n], joint sequences, there are two joints in Thumb
 % 				1, near palm
 % 				2, far from the palm
 % 			[p], relative position of the steering engine
-% 			[t], [ms] how much time to complete [p]-position
 % Output:
 	switch n
 	case 1
