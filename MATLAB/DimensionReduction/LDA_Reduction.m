52089d152d9b83b8aa9f8c63dd99b8aedfe8d7ed
zero <zerolee18@gmail.com>
2017-08-02 20:55:01 +0800

    module function of LDA

--- MATLAB/DimensionReduction/LDA_Reduction.m
+++ MATLAB/DimensionReduction/LDA_Reduction.m
@@ -1,31 +1,34 @@
 % function description
 % Input: 
-% 		Samples_Matrix, nx1-Cell, every cell is a supervised cluster.
-% 		d, number of reduction dimension.
+% 		[samplesCell], nx1-Cell, every cell is a supervised cluster.
+% 				samplesCell{n} -> [] matrix with M x (nCh x nFe)
+% 									M, number of sliding window
+% 									every row of [] is a sample in feature space. 
+% 		[d], number of reduction dimension.
 
 % Output:
-% 		centers, center of every conrresponding supervised cluster.
-% 		LDA_matrix, the dimension-reduction matrix with Principle Component Analysis.
+% 		[centers], center of every conrresponding supervised cluster.
+% 		[LDA_matrix], the dimension-reduction matrix with Principle Component Analysis.
 % Attention:
 % 		LDA algorithm do not need pre-processing.
-function [LDA_centers, LDA_matrix] = LDA_Reduction(Samples_Matrix, d)
+function [LDA_centers, LDA_matrix] = LDA_Reduction(samplesCell, d)
 	disp('LDA reduction is being conducted.');
 	% xSw
-	Width = size(Samples_Matrix{1}, 2);
+	Width = size(samplesCell{1}, 2);
 	xSw = zeros(Width);
 	SUM = zeros(1, Width);
-	for mv=1:5
-		Specimen_Counts = size(Samples_Matrix{mv}, 1);
-		center(mv, :) = mean(Samples_Matrix{mv}, 1);
-		temp = Samples_Matrix{mv} - repmat(center(mv, :), Specimen_Counts, 1);
+	for mv=1:length(samplesCell)
+		Specimen_Counts = size(samplesCell{mv}, 1);
+		center(mv, :) = mean(samplesCell{mv}, 1);
+		temp = samplesCell{mv} - repmat(center(mv, :), Specimen_Counts, 1);
 		xSw = xSw + temp'*temp;
-		SUM = SUM + sum(Samples_Matrix{mv}, 1);
+		SUM = SUM + sum(samplesCell{mv}, 1);
 	end
-	Xmeans = SUM/5/Specimen_Counts;
+	Xmeans = SUM/length(samplesCell)/Specimen_Counts;
 
 	% xSb
 	xSb = zeros(Width);
-	for mv=1:5
+	for mv=1:length(samplesCell)
 		temp = center(mv, :) - Xmeans;
 		xSb = xSb + temp'*temp;
 	end
@@ -37,3 +40,4 @@ function [LDA_centers, LDA_matrix] = LDA_Reduction(Samples_Matrix, d)
 	LDA_matrix = Vector(:, 1:d);
 
 	LDA_centers = center * LDA_matrix;
+end
